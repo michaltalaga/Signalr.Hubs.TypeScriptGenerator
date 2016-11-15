a6da5871e1dd98c58f64d2b9f98bc2bfbb225bb0
pavel.slotvinski <pavel.slotvinski@geniussports.com>
2016-11-15 14:57:00 +0200

    Increment version of assemblies (to 1.0.8.0)

--- src/Signalr.Hubs.TypescriptGenerator/Properties/AssemblyInfo.cs
+++ src/Signalr.Hubs.TypescriptGenerator/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Signalr.Hubs.TypeScriptGenerator")]
-[assembly: AssemblyDescription("Utility library for generating TypeScript hub definitions for a Signalr assembly.")]
+[assembly: AssemblyDescription("Utility library for generating TypeScript hub definitions for a SignalR assembly.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Genius Sports")]
 [assembly: AssemblyProduct("Signalr.Hubs.TypeScriptGenerator")]
 [assembly: AssemblyCopyright("Copyright © Genius Sports 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.7.0")]
-[assembly: AssemblyFileVersion("1.0.7.0")]
+[assembly: AssemblyVersion("1.0.8.0")]
+[assembly: AssemblyFileVersion("1.0.8.0")]
